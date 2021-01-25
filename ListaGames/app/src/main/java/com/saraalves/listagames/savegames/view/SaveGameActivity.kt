aea4b0ba609f3378213a752c3b9eb57bf3233bfa
Sara Alves <saraalves14@hotmail.com>
2021-01-25 17:00:34 -0300

    adicionando layout de detalhes

---
+++ ListaGames/app/src/main/java/com/saraalves/listagames/savegames/view/SaveGameActivity.kt
@@ -0,0 +1,12 @@
+package com.saraalves.listagames.savegames.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.saraalves.listagames.R
+
+class SaveGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_save_game)
+    }
+}
