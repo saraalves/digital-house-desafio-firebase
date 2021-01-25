262f9f4cfa6d79ef434027881c3569c397bedf77
Sara Alves <saraalves14@hotmail.com>
2021-01-25 18:00:26 -0300

    adicionando layout de detalhes

---
+++ ListaGames/app/src/main/java/com/saraalves/listagames/splash/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.saraalves.listagames.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.google.firebase.auth.FirebaseAuth
+import com.saraalves.listagames.R
+import com.saraalves.listagames.listagames.ListaGamesActivity
+import com.saraalves.listagames.login.view.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        auth = FirebaseAuth.getInstance()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (auth.currentUser == null) {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                val intent = Intent(this, ListaGamesActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 2000)
+    }
+}
