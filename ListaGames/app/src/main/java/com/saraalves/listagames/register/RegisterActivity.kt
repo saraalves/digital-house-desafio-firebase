61e46393e86b2b559627dfb822ca8fe7fb5b1b36
Sara Alves <saraalves14@hotmail.com>
2021-01-25 16:11:41 -0300

    cração do login e register adapter e viewholder da lista de games

--- ListaGames/app/src/main/java/com/saraalves/listagames/register/RegisterActivity.kt
+++ ListaGames/app/src/main/java/com/saraalves/listagames/register/RegisterActivity.kt
@@ -1,11 +1,12 @@
-package com.saraalves.listagames
+package com.saraalves.listagames.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.saraalves.listagames.R
 
-class CadastroGamesActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastro_games)
+        setContentView(R.layout.activity_register)
     }
 }
