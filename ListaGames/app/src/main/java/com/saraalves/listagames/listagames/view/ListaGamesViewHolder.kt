627ff72820bd4afd5af80846b2f788db6123ddf2
Sara Alves <saraalves14@hotmail.com>
2021-01-27 12:25:51 -0300

    validações de login criadas e cadastro de conta

--- ListaGames/app/src/main/java/com/saraalves/listagames/listagames/view/ListaGamesViewHolder.kt
+++ ListaGames/app/src/main/java/com/saraalves/listagames/listagames/view/ListaGamesViewHolder.kt
@@ -4,11 +4,12 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.storage.FirebaseStorage
 import com.saraalves.listagames.R
 import com.saraalves.listagames.listagames.model.GamesModel
 import com.squareup.picasso.Picasso
 
-class ListaGamesViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+class ListaGamesViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
     private val imagem: ImageView = view.findViewById(R.id.cardGame)
     private val nomeGame: TextView = view.findViewById(R.id.txtNameGame)
@@ -17,6 +18,17 @@ class ListaGamesViewHolder(private val view: View): RecyclerView.ViewHolder(view
     fun bind(game: GamesModel) {
         nomeGame.text = game.nome
         anoGame.text = game.ano
-        Picasso.get().load(game.imgUrl).into(imagem)
+        carregarImagem(game.imgUrl, imagem)
+    }
+
+    private fun carregarImagem(img: String, imagem: ImageView?) {
+        val storage = FirebaseStorage.getInstance().getReference("uploads")
+
+        storage.child(img).downloadUrl.addOnSuccessListener {
+            Picasso.get()
+                .load(it)
+                .into(imagem)
+        }
     }
 }
+
